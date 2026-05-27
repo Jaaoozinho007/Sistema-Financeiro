6791c8833488deeea9d3a5115f62035a11f1967b
Eduardo <duduaraujo.steigleder@gmail.com>
2026-05-26 21:18:46 -0300

    primeira versão - desenvolvimento mes a mes

---
+++ o desenvolvimento - mês a mês.py
@@ -0,0 +1,7 @@
+be = int(800) ##bolsa estágio 2000-1200, já descontando aluguel, internet e miojo
+sldi = int(1000) ##saldo inicial
+
+sldr = be + sldi
+
+mês = int(input("Qual mês estamos? "))
+gasto = int(input("Quanto foi o gasto esse mês: "))
